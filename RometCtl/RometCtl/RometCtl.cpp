c3d4f4c9392395cc345bb8fe0b04697f9c5d1e2f
lzy <3012265452@qq.com>
2024-03-08 16:47:32 +0800

    1.完成了服务器开机启动的功能

--- RometCtl/RometCtl/RometCtl.cpp
+++ RometCtl/RometCtl/RometCtl.cpp
@@ -18,8 +18,59 @@
 // 唯一的应用程序对象
 //分支001
 CWinApp theApp;
-
 using namespace std;
+
+void ChooseAutoInvoke() {
+    //开机启动的时候，程序的权限是跟随启动用户的
+    //如果两者权限不一致，则会导致程序启动失败
+    //开机h启动对环境变量有影响，如果以来dll（动态库），则可能启动失败
+    //解决方法：
+    //【复制这些dll到system32下面或者sysWOW64下面】
+    //system32下面，多是64为程序 sysWOW64下面多是32为程序
+    //【使用静态库，而非动态库】
+    TCHAR wcsSystem[MAX_PATH] = _T("");
+    CString strPath = CString(_T("C:\\Windows\\SysWOW64\\RometCtl.exe"));
+    if (PathFileExists(strPath)) {
+        return;
+    }
+    CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+    CString strInfo = _T("改程序只允许用于合法的用途！\n");
+    strInfo += _T("继续运行该程序，将使得这台机器处于被监视的状态！\n");
+    strInfo += _T("如果你不希望这样，请按“取消”按钮，退出程序。\n");
+    strInfo += _T("按下“是”按钮，该程序将被复制到你的机器上，并随系统启动而自动运行！\n");
+    strInfo += _T("按下“否”按钮，程序只运行一次，不会在系统内留下任何东西！\n");
+    int ret = MessageBox(NULL, strInfo, _T("警告"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
+    if (ret == IDYES) {
+        //1.建立软链接
+        char sPath[MAX_PATH]{ "" };
+        char sSys[MAX_PATH]{ "" };
+        std::string strExe{ "\\RometCtl.exe " };
+        GetCurrentDirectoryA(MAX_PATH, sPath);
+        GetSystemDirectoryA(sSys, sizeof(sSys));
+        //不用复制动态链接库
+        std::string strCmd = "cmd /K mklink " + std::string(sSys) + strExe + std::string(sPath) + strExe;
+        system(strCmd.c_str());
+        //2.建立注册表
+        HKEY hKey = NULL;
+        ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
+        if (ret != ERROR_SUCCESS) {
+            RegCloseKey(hKey);
+            MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败!"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+            exit(0);
+        }
+        ret = RegSetValueEx(hKey, _T("RometCtl"), 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));
+        if (ret != ERROR_SUCCESS) {
+            RegCloseKey(hKey);
+			MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？\r\n程序启动失败!"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+			exit(0);
+        }
+        RegCloseKey(hKey);
+    }
+    else if (ret == IDCANCEL) {
+        exit(0);
+    }
+    return;
+}
 
 int main()
 {
@@ -40,7 +91,7 @@ int main()
         {
             CCommand cmd;
             // TODO: 在此处为应用程序的行为编写代码。
-            
+            ChooseAutoInvoke();
             int ret = CServerSocket::getInstance()->Run(&CCommand::RunCommand, &cmd);//为什么还要取地址？？
             switch (ret)
             {
