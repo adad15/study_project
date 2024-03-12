182ea5a512ee5bb12aaab885a741abb075142a4d
lzy <3012265452@qq.com>
2024-03-12 17:03:03 +0800

    1.完成了rece的处理

--- RometCtl/RometCtl/MyServer.cpp
+++ RometCtl/RometCtl/MyServer.cpp
@@ -1,13 +1,14 @@
 #include "pch.h"
 #include "MyServer.h"
 #pragma warning(disable:4407)
+
 template<MyOperator op>
 AcceptOverlapped<op>::AcceptOverlapped() {
 	m_worker = ThreadWork(this, (FUNCTYPE) & AcceptOverlapped<op>::AcceptWorker);
 	m_operator = MyAccept;
 	memset(&m_overlapped, 0, sizeof(m_overlapped));
 	m_buffer.resize(1024);
-	m_server = NULL;
+	m_pserver = NULL;
 }
 
 template<MyOperator op>
@@ -18,22 +19,39 @@ int AcceptOverlapped<op>::AcceptWorker() {
 		GetAcceptExSockaddrs(*m_client, 0, sizeof(sockaddr_in) + 16, sizeof(sockaddr_in) + 16,
 			(sockaddr**)m_client->GetLocalAddr(), &lLength/*本地地址*/,
 			(sockaddr**)m_client->GetRemoteAddr(), &rLength/*远程地址*/);
-		PCLIENT pClient{ new CMyClient() };
-		//pClient->SetOverlapped(pClient);//客户端指针加入到Overlapped里面
-		//m_client.insert(std::pair<SOCKET, PCLIENT>(*pClient, pClient));
-		//if (AcceptEx(m_sock, *pClient, *pClient/*使用了operator PVOID()*/, 0, sizeof(sockaddr_in) + 16,
-		//	sizeof(sockaddr_in) + 16, *pClient/*operator LPDWORD()*/, *pClient/*operator LPOVERLAPPED()*/) == FALSE)
-		//{
-		//	return -2;
-		//}
-		if (!m_server->NewAccept()) {
+		
+		int ret = WSARecv((SOCKET)*m_client, m_client->RecvWSABuffer(), 1, *m_client, & m_client->flags(), *m_client, NULL);
+		if (ret == SOCKET_ERROR && (WSAGetLastError() != WSA_IO_PENDING)) {
+
+		}
+		if (!m_pserver->NewAccept()) {
 			return -2;
 		}
 	}
 	return -1;
 }
 
-CMyClient::CMyClient() :m_isbusy(false), m_overlapped(new ACCEPTOVERLAPPED()) {
+template<MyOperator op>
+inline RecvOverlapped<op>::RecvOverlapped() {
+	m_operator = MyRecv;
+	m_worker = ThreadWork(this, (FUNCTYPE)&RecvOverlapped<op>::RecvWorker);
+	memset(&m_overlapped, 0, sizeof(m_overlapped));
+	m_buffer.resize(1024 * 256);
+}
+
+template<MyOperator op>
+inline SendOverlapped<op>::SendOverlapped() {
+	m_operator = MySend;
+	m_worker = ThreadWork(this, (FUNCTYPE)&SendOverlapped<op>::SendWorker);
+	memset(&m_overlapped, 0, sizeof(m_overlapped));
+	m_buffer.resize(1024 * 256);
+}
+
+CMyClient::CMyClient() :m_isbusy(false), m_flags(0),
+	m_overlapped(new ACCEPTOVERLAPPED()),
+	m_recv(new RECVOVERLAPPED()),
+	m_send(new SENDOVERLAPPED())
+{
 	m_sock = WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 	m_buffer.resize(1024);
 	memset(&m_laddr, 0, sizeof(m_laddr));
@@ -41,10 +59,88 @@ CMyClient::CMyClient() :m_isbusy(false), m_overlapped(new ACCEPTOVERLAPPED()) {
 }
 void CMyClient::SetOverlapped(PCLIENT& ptr) {
 	m_overlapped->m_client = ptr;
+	m_recv->m_client = ptr;
+	m_send->m_client = ptr;
 }
 CMyClient::operator LPOVERLAPPED() {
 	return &m_overlapped->m_overlapped;
 }
-CMyClient::operator LPDWORD() {
-	return &m_received;
+
+LPWSABUF CMyClient::RecvWSABuffer()
+{
+	return &m_recv->m_wsabuffer;
+}
+LPWSABUF CMyClient::SendWSABuffer()
+{
+	return &m_send->m_wsabuffer;
+}
+
+bool CMyServer::StartServic()
+{
+	CreatSocket();
+	if (bind(m_sock, (sockaddr*)&m_addr, sizeof(m_addr)) == -1) {
+		closesocket(m_sock);
+		m_sock = INVALID_SOCKET;
+		return false;
+	}
+	if (listen(m_sock, 3) == -1) {
+		closesocket(m_sock);
+		m_sock = INVALID_SOCKET;
+		return false;
+	}
+	//完成端口映射初始化
+	m_hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 4);
+	if (m_hIOCP == NULL) {
+		closesocket(m_sock);
+		m_sock = INVALID_SOCKET;
+		m_hIOCP = INVALID_HANDLE_VALUE;
+		return false;
+	}
+	CreateIoCompletionPort((HANDLE)m_sock, m_hIOCP, (ULONG_PTR)this, 0);//绑定端口
+	m_pool.Invoke();
+	m_pool.DispatchWorker(ThreadWork(this, (FUNCTYPE)&CMyServer::threadIocp));
+	if (!NewAccept()) return false;
+	return true;
+}
+
+int CMyServer::threadIocp()
+{
+	DWORD tranferred{};
+	ULONG_PTR CompletionKey{};
+	OVERLAPPED* lpOverlapped{ NULL };
+	if (GetQueuedCompletionStatus(m_hIOCP, &tranferred, &CompletionKey, &lpOverlapped, INFINITE)) {
+		if (tranferred > 0 && (CompletionKey != 0)) {
+			CMyOverlapped* pOverlapped = CONTAINING_RECORD(lpOverlapped, CMyOverlapped, m_overlapped);
+			switch (pOverlapped->m_operator) {
+			case MyAccept:
+			{
+				ACCEPTOVERLAPPED* pOver = (ACCEPTOVERLAPPED*)pOverlapped;
+				m_pool.DispatchWorker(pOver->m_worker);
+			}
+			break;
+			case MyRecv:
+			{
+				RECVOVERLAPPED* pOver = (RECVOVERLAPPED*)pOverlapped;
+				m_pool.DispatchWorker(pOver->m_worker);
+			}
+			break;
+			case MySend:
+			{
+				SENDOVERLAPPED* pOver = (SENDOVERLAPPED*)pOverlapped;
+				m_pool.DispatchWorker(pOver->m_worker);
+			}
+			break;
+			case MyError:
+			{
+				ERROROVERLAPPED* pOver = (ERROROVERLAPPED*)pOverlapped;
+				m_pool.DispatchWorker(pOver->m_worker);
+			}
+			break;
+			}
+		}
+		else {
+			return -1;
+		}
+	}
+	return 0;
 }
--- RometCtl/RometCtl/MyServer.h
+++ RometCtl/RometCtl/MyServer.h
@@ -18,6 +18,10 @@ typedef std::shared_ptr<CMyClient> PCLIENT;
 
 template<MyOperator>class AcceptOverlapped;
 typedef AcceptOverlapped<MyAccept>ACCEPTOVERLAPPED;
+template<MyOperator>class RecvOverlapped;
+typedef RecvOverlapped<MyRecv>RECVOVERLAPPED;
+template<MyOperator>class SendOverlapped;
+typedef SendOverlapped<MySend>SENDOVERLAPPED;
 
 class CMyOverlapped {
 public:
@@ -25,7 +29,9 @@ public:
     DWORD m_operator;          //操作,参见MyOperator
     std::vector<char> m_buffer;//缓冲区
     ThreadWork m_worker;//处理函数
-    CMyServer* m_server;//服务器对象
+    CMyServer* m_pserver;//服务器对象
+	PCLIENT m_client;//对应的客户端
+	WSABUF m_wsabuffer;
 };
 
 class CMyClient {
@@ -34,23 +40,48 @@ public:
 	~CMyClient() {
 		closesocket(m_sock);
 	}
+	//传递参数，CMyClient类指针,设置
 	void SetOverlapped(PCLIENT& ptr);
-	//??????????????强制数据类型转换？？？？？？？？？？？？？
+	//强制类型转换，将CMyClient类转换为SOCKET类型的m_sock
 	operator SOCKET() {
 		return m_sock;
 	}
-	operator PVOID() {
+	operator PVOID() 
+	{
 		return &m_buffer[0];
 	}
 	operator LPOVERLAPPED();
-	operator LPDWORD();
+	operator LPDWORD() 
+	{
+		return &m_received;
+	}
+
+	LPWSABUF RecvWSABuffer();
+	LPWSABUF SendWSABuffer();
+
+	DWORD& flags() { return m_flags; }
+
 	sockaddr_in* GetLocalAddr() { return &m_laddr; }
 	sockaddr_in* GetRemoteAddr() { return &m_raddr; }
+	size_t GetBufferSize() const{
+		return m_buffer.size();
+	}
+	int Recv() {
+		int ret = recv(m_sock, m_buffer.data() + m_used, m_buffer.size() - m_used, 0);
+		if (ret <= 0) return -1;
+		m_used += (size_t)ret;
+		//TODO 解析数据还没有完成
+		return 0;
+	}
 private:
 	SOCKET m_sock;
 	DWORD m_received;
-	std::shared_ptr< ACCEPTOVERLAPPED> m_overlapped;
+	DWORD m_flags;
+	std::shared_ptr<ACCEPTOVERLAPPED> m_overlapped;
+	std::shared_ptr<RECVOVERLAPPED> m_recv;
+	std::shared_ptr<SENDOVERLAPPED> m_send;
 	std::vector<char>m_buffer;
+	size_t m_used;//已经使用的缓冲区的大小
 	sockaddr_in m_laddr;
 	sockaddr_in m_raddr;
 	bool m_isbusy;
@@ -61,34 +92,26 @@ class AcceptOverlapped :public CMyOverlapped, ThreadFuncBase {
 public:
 	AcceptOverlapped();
 	int AcceptWorker();
-    PCLIENT m_client;
 };
 
 template<MyOperator>
 class RecvOverlapped :public CMyOverlapped, ThreadFuncBase {
 public:
-    RecvOverlapped() :m_operator(MyRecv), m_worker(this, &RecvOverlapped::RecvWorker) {
-		memset(&m_overlapped, 0, sizeof(m_overlapped));
-        m_buffer.resize(1024 * 256);
-	}
+	RecvOverlapped();
 	int RecvWorker() {
-
+		int ret = m_client->Recv();
+		return ret;
 	}
 };
-typedef RecvOverlapped<MyRecv>RECVOVERLAPPED;
 
 template<MyOperator>
 class SendOverlapped :public CMyOverlapped, ThreadFuncBase {
 public:
-    SendOverlapped() :m_operator(MySend), m_worker(this, &SendOverlapped::SendWorker) {
-		memset(&m_overlapped, 0, sizeof(m_overlapped));
-        m_buffer.resize(1024 * 256);
-	}
+	SendOverlapped();
 	int SendWorker() {
-
+		return -1;
 	}
 };
-typedef SendOverlapped<MySend>SENDOVERLAPPED;
 
 template<MyOperator>
 class ErrorOverlapped :public CMyOverlapped, ThreadFuncBase {
@@ -98,7 +121,7 @@ public:
 		m_buffer.resize(1024);
 	}
 	int ErrorWorker() {
-
+		return -1;
 	}
 };
 typedef ErrorOverlapped<MyError>ERROROVERLAPPED;
@@ -114,32 +137,7 @@ public:
 		m_addr.sin_addr.s_addr = inet_addr(ip.c_str());
     }
     ~CMyServer() {}
-    bool StartServic() {
-		CreatSocket();
-		if (bind(m_sock, (sockaddr*)&m_addr, sizeof(m_addr)) == -1) {
-			closesocket(m_sock);
-			m_sock = INVALID_SOCKET;
-			return false;
-		}
-		if (listen(m_sock, 3) == -1) {
-			closesocket(m_sock);
-			m_sock = INVALID_SOCKET;
-			return false;
-		}
-		//完成端口映射初始化
-		m_hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 4);
-		if (m_hIOCP == NULL) {
-			closesocket(m_sock);
-			m_sock = INVALID_SOCKET;
-			m_hIOCP = INVALID_HANDLE_VALUE;
-			return false;
-		}
-		CreateIoCompletionPort((HANDLE)m_sock, m_hIOCP, (ULONG_PTR)this, 0);//绑定端口
-		m_pool.Invoke();
-		m_pool.DispatchWorker(ThreadWork(this, (FUNCTYPE)&CMyServer::threadIocp));
-		if (!NewAccept()) return false;
-		return true;
-    }
+	bool StartServic();
 	bool NewAccept() {
 		PCLIENT pClient{ new CMyClient() };
 		pClient->SetOverlapped(pClient);//客户端指针加入到Overlapped里面
@@ -162,46 +160,7 @@ private:
 		//设置套接字属性
 		setsockopt(m_sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt));
 	}
-    int threadIocp() {
-        DWORD tranferred{};
-        ULONG_PTR CompletionKey{};
-        OVERLAPPED* lpOverlapped{ NULL };
-        if (GetQueuedCompletionStatus(m_hIOCP, &tranferred, &CompletionKey, &lpOverlapped, INFINITE)) {
-            if (tranferred > 0 && (CompletionKey != 0)) {
-				CMyOverlapped* pOverlapped = CONTAINING_RECORD(lpOverlapped, CMyOverlapped, m_overlapped);
-				switch (pOverlapped->m_operator) {
-				case MyAccept:
-				{
-					ACCEPTOVERLAPPED* pOver = (ACCEPTOVERLAPPED*)pOverlapped;
-					m_pool.DispatchWorker(pOver->m_worker);
-				}
-				break;
-				case MyRecv:
-				{
-					RECVOVERLAPPED* pOver = (RECVOVERLAPPED*)pOverlapped;
-					m_pool.DispatchWorker(pOver->m_worker);
-				}
-				break;
-				case MySend:
-				{
-					SENDOVERLAPPED* pOver = (SENDOVERLAPPED*)pOverlapped;
-					m_pool.DispatchWorker(pOver->m_worker);
-				}
-				break;
-				case MyError:
-				{
-					ERROROVERLAPPED* pOver = (ERROROVERLAPPED*)pOverlapped;
-					m_pool.DispatchWorker(pOver->m_worker);
-				}
-				break;
-				}
-            }
-            else {
-                return -1;
-            }
-        }
-        return 0;
-    }
+	int threadIocp();
 private:
     CMyThreadPool m_pool;
     HANDLE m_hIOCP;
